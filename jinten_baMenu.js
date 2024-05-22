c4d4be9137e25d919b71c924cb7aef5c2fc32d8c
tonybala <tonybala@gmail.com>
2024-05-22 20:07:13 +0800

    Update jinten_baMenu.js
    keyword

--- jinten_baMenu.js
+++ jinten_baMenu.js
@@ -5,7 +5,7 @@
 		s+='        <div class="container">                                                                              ' ;
 		s+='            <div class="row mt-5 mb-3 align-items-center">                                                   ' ;
 		s+='                <div class="col-md-4">                                                                       ' ;
-		s+='                歡迎多加利用搜尋功能,可用任一字眼快速查詢產品                                                        ' ;
+		s+='                歡迎多加利用搜尋功能,可用任一字眼快速查詢所有產品                                                        ' ;
 		s+='                </div>                                                                                       ' ;
 		s+='                <div class="col-md-4">                                                                       ' ;
 		s+='                  <input type="text" class="form-control" placeholder="請在此輸入關鍵字..." id="ba_jinten_menu2_searchField"> ' ;
@@ -28,10 +28,10 @@
 		s+='        </div>                                                                                               ' ;
 		s+='    </div>                                                                                                   ' ;
 		s+='    <style>                                                                                                   ' ;
-		s+='      #ba_jinten_menu2 .btn-primary { width:24px;height:24px }   ' ;
-		s+='      #ba_jinten_menu2 .btn-default  { width:24px;height:24px }  ' ;
-		s+='      #ba_jinten_menu2 > div > table > tbody  a {cursor:zoom-in}  ' ;
-		s+='    </style>                                                     ' ;
+		s+='      #ba_jinten_menu2 .btn-primary { width:24px;height:24px } ' ;
+		s+='      #ba_jinten_menu2 .btn-default  { width:24px;height:24px }' ;
+		s+='      #ba_jinten_menu2 > div > table > tbody a {cursor:zoom-in}' ;
+		s+='    </style>                                                   ' ;
 		return s;
 	}
 	function initBaMenu() {
