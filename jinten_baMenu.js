985103f8a1ee8f01ffa5e7b48af60be9aeeb3887
tonybala <tonybala@gmail.com>
2024-05-23 00:17:20 +0800

    change pdf icon position

--- jinten_baMenu.js
+++ jinten_baMenu.js
@@ -1,41 +1,41 @@
 (function () {
 	function boostrap_grid_html() {
-		s = '' ; 
-		s+='    <div class="page-container">                                                                             ' ;
-		s+='        <div class="container">                                                                              ' ;
-		s+='            <div class="row mt-5 mb-3 align-items-center">                                                   ' ;
-		s+='                <div class="col-md-4">                                                                       ' ;
-		s+='                歡迎多加利用搜尋功能,可用任一字眼快速查詢所有產品                                                        ' ;
-		s+='                </div>                                                                                       ' ;
-		s+='                <div class="col-md-4">                                                                       ' ;
-		s+='                  <input type="text" class="form-control" placeholder="請在此輸入關鍵字..." id="ba_jinten_menu2_searchField"> ' ;
-		s+='                </div>                                                                                       ' ;
-		s+='                <div class="col-md-2 text-right">                                                            ' ;
-		s+='                  <span class="pr-3">每頁顯示筆數:</span>                                                   ' ;
-		s+='                </div>                                                                                       ' ;
-		s+='                <div class="col-md-2">                                                                       ' ;
-		s+='                    <div class="d-flex justify-content-end">                                                 ' ;
-		s+='                        <select class="custom-select" name="rowsPerPage" id="changeRows">                    ' ;
-		s+='                            <option value="1">1</option>                                                     ' ;
-		s+='                            <option value="5" selected>5</option>                                            ' ;
-		s+='                            <option value="10">10</option>                                                   ' ;
-		s+='                            <option value="15">15</option>                                                   ' ;
-		s+='                        </select>                                                                            ' ;
-		s+='                    </div>                                                                                   ' ;
-		s+='                </div>                                                                                       ' ;
-		s+='            </div>                                                                                           ' ;
-		s+='            <div id="ba_jinten_menu2"></div>                                                                            ' ;
-		s+='        </div>                                                                                               ' ;
-		s+='    </div>                                                                                                   ' ;
-		s+='    <style>                                                                                                   ' ;
-		s+='      #ba_jinten_menu2 .btn-primary { width:24px;height:24px } ' ;
-		s+='      #ba_jinten_menu2 .btn-default  { width:24px;height:24px }' ;
-		s+='      #ba_jinten_menu2 > div > table > tbody a {cursor:zoom-in}' ;
-		s+='    </style>                                                   ' ;
+		s = '';
+		s += '    <div class="page-container">                                                                             ';
+		s += '        <div class="container">                                                                              ';
+		s += '            <div class="row mt-5 mb-3 align-items-center">                                                   ';
+		s += '                <div class="col-md-4">                                                                       ';
+		s += '                歡迎多加利用搜尋功能,可用任一字眼快速查詢所有產品                                                        ';
+		s += '                </div>                                                                                       ';
+		s += '                <div class="col-md-4">                                                                       ';
+		s += '                  <input type="text" class="form-control" placeholder="請在此輸入關鍵字..." id="ba_jinten_menu2_searchField"> ';
+		s += '                </div>                                                                                       ';
+		s += '                <div class="col-md-2 text-right">                                                            ';
+		s += '                  <span class="pr-3">每頁顯示筆數:</span>                                                   ';
+		s += '                </div>                                                                                       ';
+		s += '                <div class="col-md-2">                                                                       ';
+		s += '                    <div class="d-flex justify-content-end">                                                 ';
+		s += '                        <select class="custom-select" name="rowsPerPage" id="changeRows">                    ';
+		s += '                            <option value="1">1</option>                                                     ';
+		s += '                            <option value="5" selected>5</option>                                            ';
+		s += '                            <option value="10">10</option>                                                   ';
+		s += '                            <option value="15">15</option>                                                   ';
+		s += '                        </select>                                                                            ';
+		s += '                    </div>                                                                                   ';
+		s += '                </div>                                                                                       ';
+		s += '            </div>                                                                                           ';
+		s += '            <div id="ba_jinten_menu2"></div>                                                                            ';
+		s += '        </div>                                                                                               ';
+		s += '    </div>                                                                                                   ';
+		s += '    <style>                                                                                                   ';
+		s += '      #ba_jinten_menu2 .btn-primary { width:24px;height:24px } ';
+		s += '      #ba_jinten_menu2 .btn-default  { width:24px;height:24px }';
+		s += '      #ba_jinten_menu2 > div > table > tbody a {cursor:zoom-in}';
+		s += '    </style>                                                   ';
 		return s;
 	}
 	function initBaMenu() {
-		$('#content > div > div.row').after( boostrap_grid_html() );
+		$('#content > div > div.row').after(boostrap_grid_html());
 		//console.log('initBaMenu 2');
 		var baTable2 = $('#ba_jinten_menu2').tableSortable({
 			data: ba_jinten_menu_items2,
@@ -72,28 +72,28 @@
 					if (row["檢知管編號文件網址"] == "")
 						return $('<span></span>').text(row[key]);
 					else
-						return $('<a href="' + row["檢知管編號文件網址"] + '">' + row[key] +doPdf2+ '</a>');
+						return $('<a href="' + row["檢知管編號文件網址"] + '">' +doPdf2+ row[key] + '</a>');
 				}
 				if (key === '檢知管類型') {
 					if (row["檢知管類型文件網址"] == "")
 						return $('<span></span>').text(row[key]);
 					else
-						return $('<a href="' + row["檢知管類型文件網址"] + '">' + row[key] +doPdf2+ '</a>');
+						return $('<a href="' + row["檢知管類型文件網址"] + '">' + doPdf2 + row[key] + '</a>');
 				}
 				if (key === '說明') {
 					if (row["說明文件網址"] == "")
 						return $('<span></span>').text(row[key]);
 					else
-						return $('<a href="' + row["說明文件網址"] + '">' + row[key] +doPdf2+ '</a>');
+						return $('<a href="' + row["說明文件網址"] + '">' + doPdf2 + row[key] + '</a>');
 				}
 				// Finally return cell for rest of columns;
 				return row[key];
 			},
 		});
 
-		$('#changeRows').on('change', function() {
+		$('#changeRows').on('change', function () {
 			baTable2.updateRowsPerPage(parseInt($(this).val(), 10));
-		})		
+		})
 
 	}
 	debugger;
